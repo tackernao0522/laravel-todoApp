bd4e0d73143401aedd2464357953a0b9539bbbac
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2020-11-18 13:14:30 +0900

    TaskController@indexの編集及びスタイルシートの作成とインデックスビューの編集

--- laravel/resources/views/tasks/index.blade.php
+++ laravel/resources/views/tasks/index.blade.php
@@ -28,7 +28,7 @@
             </div>
             <div class="list-group">
               @foreach ($folders as $folder)
-              <a href="{{ route('tasks.index', ['id' => $folder->id]) }}">
+              <a href="{{ route('tasks.index', ['id' => $folder->id]) }}" class="list-group-item {{ $current_folder_id === $folder->id ? 'active' : '' }}">
                 {{ $folder->title }}
               </a>
               @endforeach
