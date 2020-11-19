105555773d4b6e86aa4fc43b4748276acae5337b
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2020-11-19 15:40:35 +0900

    状態の名前を表示する

--- laravel/app/Task.php
+++ laravel/app/Task.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    /**
+     * 状態定義
+     */
+    const STATUS = [
+        1 => ['label' => '未着手'],
+        2 => ['label' => '着手中'],
+        3 => ['label' => '完了'],
+    ];
+
+    /**
+     * 状態のラベル
+     * @return string
+     */
+    public function getStatusLabelAttribute()
+    {
+        // 状態値
+        $status = $this->attributes['status'];
+
+        // 定義されていなければ空文字を返す
+        if (!isset(self::STATUS[$status])) {
+            return '';
+        }
+
+        return self::STATUS[$status]['label'];
+    }
 }
