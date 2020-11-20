b36fe8f4c3a266ab4eae292e4181e37a09d88df6
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2020-11-20 13:47:53 +0900

    validationの作成

--- laravel/resources/views/folders/create.blade.php
+++ laravel/resources/views/folders/create.blade.php
@@ -22,6 +22,15 @@
           <nav class="panel panel-default">
             <div class="panel-heading">フォルダを追加する</div>
             <div class="panel-body">
+              @if ($errors->any())
+              <div class="alert alert-danger">
+                <ul>
+                  @foreach ($errors->all() as $message)
+                  <li>{{ $message }}</li>
+                  @endforeach
+                </ul>
+              </div>
+              @endif
               <form action="{{ route('folders.create') }}" method="POST">
                 @csrf
                 <div class="form-group">
