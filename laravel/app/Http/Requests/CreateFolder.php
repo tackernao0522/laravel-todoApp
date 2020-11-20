a67fc5c889aedf2a7e4ac0f471383420282e6a1c
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2020-11-20 14:09:17 +0900

    エラーメッセージの日本語化

--- laravel/app/Http/Requests/CreateFolder.php
+++ laravel/app/Http/Requests/CreateFolder.php
@@ -27,4 +27,11 @@ class CreateFolder extends FormRequest
             'title' => 'required',
         ];
     }
+
+    public function attributes()
+    {
+        return [
+            'title' => 'フォルダ名',
+        ];
+    }
 }
