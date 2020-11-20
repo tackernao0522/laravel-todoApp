bd5bfe99bb2be2093a4fb8cf93da4a1007a309b8
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2020-11-20 13:34:13 +0900

    FolderConteollerにcreateアクションの作成 tasks/index.blade.phpの編集

--- laravel/app/Http/Controllers/FolderController.php
+++ laravel/app/Http/Controllers/FolderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Folder;
 use Illuminate\Http\Request;
 
 class FolderController extends Controller
@@ -9,5 +10,19 @@ class FolderController extends Controller
     public function showCreateForm()
     {
         return view('folders/create');
+    }
+
+    public function create(Request $request)
+    {
+        // フォルダモデルのインスタンスを作成
+        $folder = new Folder();
+        // タイトルに入力値を代入する
+        $folder->title = $request->title;
+        // インスタンスの状態をデータベースに書き込む
+        $folder->save();
+
+        return redirect()->route('tasks.index', [
+            'id' => $folder->id,
+        ]);
     }
 }
