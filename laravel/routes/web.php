7a732ade8a87f853464e4532e67a0f015fc65129
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2020-11-23 11:49:12 +0900

    トップページ(Home)の作成 HomeControllerの実装 ルーティングの設定

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -7,3 +7,4 @@ Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name(
 Route::post('/folders/{id}/tasks/create', 'TaskController@create');
 Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
 Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
+Route::get('/', 'HomeController@index')->name('home');
